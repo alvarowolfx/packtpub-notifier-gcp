9f9e8c2466f928caa18f9a8a7161de08a80667df
Alvaro Viebrantz <alvarowolfx@gmail.com>
2017-03-20 00:04:29 -0400

    [feat] Add firebase integration with notifications and analytics

--- mobile/PacktPubNotifier/App.js
+++ mobile/PacktPubNotifier/App.js
@@ -1,27 +1,73 @@
-import Expo, {
-  Components
-} from 'expo';
-const { BlurView } = Components;
 import React from 'react';
-import { StyleSheet, Image, View, Linking } from 'react-native';
+import { AppState, StatusBar, AsyncStorage, StyleSheet, Image, View, Linking } from 'react-native';
 import * as Animatable from 'react-native-animatable';
-
+import Firestack from 'react-native-firestack';
+import FCM from 'react-native-fcm';
 import { Spinner, Button, Icon, Text, Card, CardItem } from 'native-base';
+
+const STORAGE_KEY = 'PACKT_PUB_NOTIFIER_STATE_KEY';
+const TOPIC_NAME = 'receive_book_notification'
+
+console.disableYellowBox = true;
+const firestack = new Firestack();
 
 export default class App extends React.Component {
   state = {
-    isReady: false,
     books: [],
-    currentBookIndex: 0
+    currentBookIndex: 0,
+    wantsToReceiveNotifications: false
   }
 
   async componentWillMount() {
+    StatusBar.setBarStyle('light-content');
+    AppState.addEventListener('change', this.handleStateChange);
+
     try {
+      let savedState = await AsyncStorage.getItem(STORAGE_KEY);
+      if (savedState) {
+        this.setState(JSON.parse(savedState), () => {
+          this.fetchBooks();
+        });
+      } else {
+        this.fetchBooks();
+      }
+
+    } catch (e) {
+      this.fetchBooks();
+    }
+
+    firestack.analytics.logEventWithName("launch");
+  }
+
+  async componentDidUpdate() {
+    let stateCopy = { ...this.state };
+    delete stateCopy.currentBookIndex;
+    AsyncStorage.mergeItem(STORAGE_KEY, JSON.stringify(stateCopy));
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this.handleStateChange);
+  }
+
+  handleStateChange = (appState) => {
+    if (appState === 'active') {
+      this.fetchBooks();
+      firestack.analytics.logEventWithName("reopen");
+    }
+  }
+
+  async fetchBooks() {
+    try {
+      StatusBar.setNetworkActivityIndicatorVisible(true);
       let res = await fetch('https://iot-bootcamp-158521.appspot.com/api/books');
       let json = await res.json();
-      this.setState({ isReady: true, books: json.books, currentBookIndex: 0 });
-    } catch (e) {
-      this.setState({ isReady: true });
+      let currentBookIndex = this.state.currentBookIndex;
+      if (this.state.books.length !== json.books.length) {
+        currentBookIndex = 0;
+      }
+      this.setState({ books: json.books, currentBookIndex });
+    } finally {
+      StatusBar.setNetworkActivityIndicatorVisible(false);
     }
   }
 
@@ -30,15 +76,15 @@ export default class App extends React.Component {
       <View style={styles.card}>
         <Animatable.View style={styles.emptyCardImg}
           animation="pulse" easing="ease-out" iterationCount="infinite">
-          <Spinner />
+          <Spinner primary />
         </Animatable.View>
-        <Card style={{ marginTop: 20 }}>
+        <Card style={{ marginTop: 20, flex: 0 }}>
           <CardItem cardBody style={styles.cardBody}>
             <Text style={styles.cardTitle}>
-              Carregando livro do dia...
+              Fetching book of the day...
             </Text>
             <View>
-              <Spinner />
+              <Spinner primary />
             </View>
           </CardItem>
         </Card>
@@ -55,24 +101,29 @@ export default class App extends React.Component {
       <View key={book.slug} style={styles.card}>
         <Animatable.Image source={{ uri: book.img }} style={styles.cardImg}
           animation="pulse" easing="ease-out" iterationCount="infinite" />
-        <Card style={{ marginTop: 20 }}>
+        <Card style={{ marginTop: 20, flex: 0 }} >
           <CardItem cardBody style={styles.cardBody}>
-
             <View>
               <Text style={styles.cardTitle}>
                 {book.title || 'Sem titulo'}
               </Text>
-              <Text style={styles.cardSubtitle}>{book.description || 'Sem descrição'}</Text>
+              <Text style={styles.cardDate}>
+                {new Date(book.date).toLocaleDateString()}
+              </Text>
+              <Text numberOfLines={4}
+                style={styles.cardSubtitle}>
+                {book.description || 'Sem descrição'}
+              </Text>
             </View>
 
             <View style={styles.buttonRow}>
               <Button primary style={styles.claimButton} onPress={() => this.openLink(book.link)}>
                 <Icon name="link" />
-                <Text>Ver livro</Text>
+                <Text>See book</Text>
               </Button>
               {claim && <Button success style={styles.claimButton} onPress={() => this.openLink(book.claimLink)}>
                 <Icon name="heart" active />
-                <Text>Obter livro</Text>
+                <Text>Claim book</Text>
               </Button>}
             </View>
 
@@ -83,25 +134,32 @@ export default class App extends React.Component {
   }
 
   renderSubscribeButton() {
+    let { wantsToReceiveNotifications } = this.state;
+    const subscribe = async () => {
+      if (!wantsToReceiveNotifications) {
+        FCM.requestPermissions();
+        FCM.subscribeToTopic(TOPIC_NAME);
+        firestack.analytics.logEventWithName('subscribe');
+      } else {
+        FCM.unsubscribeFromTopic(TOPIC_NAME);
+        firestack.analytics.logEventWithName('unsubscribe');
+      }
+      this.setState({ wantsToReceiveNotifications: !wantsToReceiveNotifications })
+    };
     return (
-      <Button success style={styles.claimButton} >
-        <Icon name="notifications" />
-        <Text>Quero receber notificações</Text>
+      <Button danger={wantsToReceiveNotifications}
+        success={!wantsToReceiveNotifications}
+        style={styles.claimButton} onPress={subscribe}>
+        <Icon name="notifications" active={wantsToReceiveNotifications} />
+        <Text>{wantsToReceiveNotifications ? 'Stop' : 'Receive'} notifications</Text>
       </Button>
     );
-  }
 
-  renderContent() {
-    let { currentBookIndex, books, isReady } = this.state;
-    if (!isReady) {
-      return this.renderEmptyBook();
-    }
-    let currentBook = books[currentBookIndex];
-    return this.renderBook(currentBook, currentBookIndex === 0);
   }
 
   renderNextButton() {
     const next = (state) => {
+      firestack.analytics.logEventWithName("nextbook");
       return {
         currentBookIndex: state.currentBookIndex + 1
       };
@@ -115,6 +173,7 @@ export default class App extends React.Component {
 
   renderPrevButton() {
     const prev = (state) => {
+      firestack.analytics.logEventWithName("prevbook");
       return {
         currentBookIndex: state.currentBookIndex - 1
       };
@@ -139,17 +198,28 @@ export default class App extends React.Component {
     )
   }
 
-  render() {
-    let { books, isReady, currentBookIndex } = this.state;
+  renderContent() {
+    let { currentBookIndex, books } = this.state;
     let currentBook = books[currentBookIndex];
+    if (!!!currentBook) {
+      return this.renderEmptyBook();
+    }
+    return this.renderBook(currentBook, currentBookIndex === 0);
+  }
+
+  render() {
+    let { books, currentBookIndex } = this.state;
+    let currentBook = books[currentBookIndex];
+    let loaded = !!currentBook;
     return (
       <View style={styles.container}>
-        {isReady && <Image source={{ uri: currentBook.img }} style={StyleSheet.absoluteFill} />}
-        <BlurView tint="default" intensity={100} style={StyleSheet.absoluteFill} />
+        {loaded && <Image source={{ uri: currentBook.img }}
+          blurRadius={8}
+          style={StyleSheet.absoluteFill} />}
 
         <View style={{ flex: 1, padding: 8, paddingTop: 60, backgroundColor: 'transparent' }}>
           {this.renderContent()}
-          {isReady && this.renderButtons()}
+          {loaded && this.renderButtons()}
         </View>
       </View>
     );
@@ -162,9 +232,9 @@ const styles = {
     backgroundColor: '#cacaca',
   },
   card: {
-    flex: 1,
+    flex: 10,
     flexDirection: 'column',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
     shadowColor: 'black',
     shadowOffset: {
       width: 5,
@@ -176,7 +246,7 @@ const styles = {
   },
   cardImg: {
     flex: 1,
-    minHeight: 250,
+    minHeight: 180,
     maxHeight: 280,
     resizeMode: 'contain'
   },
@@ -184,20 +254,27 @@ const styles = {
     alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'center',
-    height: 300,
-    width: 200,
+    height: 200,
+    width: 300,
     backgroundColor: 'white'
   },
   cardTitle: {
+    flex: 0,
     fontWeight: 'bold',
     fontSize: 18,
+  },
+  cardDate: {
+    fontSize: 10,
+    fontWeight: 'bold',
+    margin: 0,
     marginBottom: 10
   },
   cardSubtitle: {
-    fontSize: 14
+    fontSize: 14,
+    marginBottom: 10
   },
   cardBody: {
-    flex: 1,
+    flex: 0,
     flexDirection: 'column',
     justifyContent: 'space-between',
     padding: 7
@@ -205,9 +282,10 @@ const styles = {
   claimButton: {
     alignSelf: 'stretch',
     alignItems: 'center',
+    padding: 11,
   },
   buttonRow: {
-    margin: 5,
+    marginVertical: 5,
     alignSelf: 'stretch',
     flexDirection: 'row',
     justifyContent: 'space-around'
